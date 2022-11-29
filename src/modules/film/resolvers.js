bec443aea7c7bcd34dd3de99620722bac2ef146e
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-11-29 22:43:30 +0100

    Thing with categories resolver

--- src/modules/film/resolvers.js
+++ src/modules/film/resolvers.js
@@ -1,14 +1,34 @@
 const SQL = require("@nearform/sql");
-
 module.exports = {
   Query: {
     getFilmList: async (parent, params, context, info) => {
-      const query = `SELECT * FROM film LIMIT ${params.limit}`;
+      const limit = params.input.resultPerPage || 5;
+      const offset = (params.input.page || 0) * limit;
+
+      const query = SQL`
+        SELECT * from film as f
+        LEFT JOIN film_category as fg ON fg.film_id = f.film_id
+        ORDER BY f.film_id
+        LIMIT ${limit}
+        OFFSET ${offset}
+      `;
+
       const result = await context.app.pg.query(query);
+
       return result.rows;
     },
   },
   Film: {
     id: (parent) => parent.film_id,
+    categories: async (parent, params, context, info) => {
+      const query = SQL`
+        SELECT * FROM category as c 
+        WHERE c.category_id = ${parent.category_id}
+      `;
+
+      const result = await context.app.pg.query(query);
+
+      return result.rows;
+    },
   },
 };
--- src/modules/hello/resolvers.js
+++ src/modules/hello/resolvers.js
@@ -1,6 +1,5 @@
 module.exports = {
   Query: {
-    hello: (parent, params, context, info) => `Hello ${params.name}`,
-    add: (parent, params, context, info) => params.num1 + params.num2,
-  },
-};
+    hello: (parent, params, context, info) => `Hello ${params.name}`
+  }
+}
