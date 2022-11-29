691b9fd410b676d11101f4e33930f9eb81886056
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-11-29 20:07:56 +0100

    Adding resolver for Film.id Using parent.film_id

--- NOTE.md
+++ NOTE.md
@@ -5,7 +5,7 @@ Subscription, interessante per LiveUX?
 Resolver senza query type crasha
 Query senza resolver è bene
 
-Parent
-Params
+Parent - e.g. l'oggetto che il DB ha restituito
+Params - i params che ho passato
 Context - uno per ogni query
 Info - la query... (not interesting here)
--- src/modules/hello/resolvers.js
+++ src/modules/hello/resolvers.js
@@ -1,22 +1,16 @@
+const SQL = require("@nearform/sql");
+
 module.exports = {
   Query: {
     hello: (parent, params, context, info) => `Hello ${params.name}`,
     add: (parent, params, context, info) => params.num1 + params.num2,
-    getFilmList: (parent, params, context, info) => {
-      console.log(context);
-      return [
-        {
-          id: 1,
-          description: "A devil rapes an advocate",
-          title: "The devil's advocate",
-        },
-        {
-          id: 2,
-          description:
-            "Gente scazzotta molto e si sfida a gare di birra e salsiccia per una Dune Buggy",
-          title: "Altrimenti ci arrabbiamo",
-        },
-      ];
+    getFilmList: async (parent, params, context, info) => {
+      const query = `SELECT * FROM film LIMIT 5`;
+      const result = await context.app.pg.query(query);
+      return result.rows;
     },
+  },
+  Film: {
+    id: (parent) => parent.film_id,
   },
 };
