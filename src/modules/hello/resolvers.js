82e55e794dc2f7adcc5f23f0cecf33c7fbbd88ec
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-11-29 19:54:42 +0100

    Logging the context

--- src/modules/hello/resolvers.js
+++ src/modules/hello/resolvers.js
@@ -2,17 +2,21 @@ module.exports = {
   Query: {
     hello: (parent, params, context, info) => `Hello ${params.name}`,
     add: (parent, params, context, info) => params.num1 + params.num2,
-    getFilmList: (parent, params, context, info) => [
-      {
-        id: 1,
-        description: "A devil rapes an advocate",
-        title: "The devil's advocate",
-      },
-      {
-        id: 2,
-        description: "Gente scazzotta molto e si sfida a gare di birra e salsiccia per una Dune Buggy",
-        title: "Altrimenti ci arrabbiamo",
-      },
-    ],
+    getFilmList: (parent, params, context, info) => {
+      console.log(context);
+      return [
+        {
+          id: 1,
+          description: "A devil rapes an advocate",
+          title: "The devil's advocate",
+        },
+        {
+          id: 2,
+          description:
+            "Gente scazzotta molto e si sfida a gare di birra e salsiccia per una Dune Buggy",
+          title: "Altrimenti ci arrabbiamo",
+        },
+      ];
+    },
   },
 };
