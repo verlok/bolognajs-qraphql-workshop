b77b8b217b5b2fdc61364526779eb143d029571e
Andrea Verlicchi <andrea.verlicchi@netcentric.biz>
2022-11-29 19:49:05 +0100

    Created resolver for Film

---
+++ NOTE.md
@@ -0,0 +1,7 @@
+# NOTE
+
+Subscription, interessante per LiveUX?
+
+Resolver senza query type crasha
+Query senza resolver è bene
+
--- src/modules/hello/resolvers.js
+++ src/modules/hello/resolvers.js
@@ -1,5 +1,18 @@
 module.exports = {
   Query: {
-    hello: (parent, params, context, info) => `Hello ${params.name}`
-  }
-}
+    hello: (parent, params, context, info) => `Hello ${params.name}`,
+    add: (parent, params, context, info) => params.num1 + params.num2,
+    getFilmList: (parent, params, context, info) => [
+      {
+        id: 1,
+        description: "A devil rapes an advocate",
+        title: "The devil's advocate",
+      },
+      {
+        id: 2,
+        description: "Gente scazzotta molto e si sfida a gare di birra e salsiccia per una Dune Buggy",
+        title: "Altrimenti ci arrabbiamo",
+      },
+    ],
+  },
+};
